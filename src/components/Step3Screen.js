3b100f15bfefa76025b83cc02c7ff0c45f559381
amanthukral12 <thukral618@gmail.com>
2022-08-18 13:38:31 +0530

    Adding layout for page 3

--- src/components/Step3Screen.js
+++ src/components/Step3Screen.js
@@ -6,7 +6,18 @@ const Step3Screen = () => {
     title="How are you planning to use Eden?"
     description="We'll streamline your setup experience accordingly."
 >
-    Screen3
+    <div>
+        <div>
+            <p>Icon1</p>
+            <h4>For myself</h4>
+            <p>Write better. Think more clearly. Stay organized.</p>
+        </div>
+        <div>
+            <p>Icon2</p>
+            <h4>With my team</h4>
+            <p>Wikis, docs, tasks & projects, all in one place.</p>
+        </div>
+    </div>
 </Base>
   )
 }
