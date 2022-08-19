b3b8f42a6ccb34c855c6b2ef14a6297e20f214dc
amanthukral12 <thukral618@gmail.com>
2022-08-19 23:04:23 +0530

    Adding step4 screen

--- src/components/Step4Screen.js
+++ src/components/Step4Screen.js
@@ -1,15 +1,12 @@
 import React from "react";
-import Base from "./Base";
+import "./styles/Base.css";
+import check from "../assets/check.svg";
 const Step4Screen = ({ formData, setFormData }) => {
   console.log(formData);
   return (
     <div>
-      <p>Tick Icon</p>
-      {/* <Base
-        title=`Congratulations, ${formData.displayName}!``
-        description="You have completed onboarding, you can start using Eden!"
-      ></Base> */}
-      <h1>Congratulations, {formData.displayName}</h1>
+      <img src={check} alt="check icon" className="icon" />
+      <h1 className="title">Congratulations, {formData.displayName}</h1>
     </div>
   );
 };
