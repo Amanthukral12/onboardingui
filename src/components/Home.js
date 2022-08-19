d8a1025014ed28b5ee0860539acb99abb05f489b
amanthukral12 <thukral618@gmail.com>
2022-08-19 23:02:22 +0530

    Adding Home page component and its styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,9 @@ import Step1Screen from "./Step1Screen";
 import Step2Screen from "./Step2Screen";
 import Step3Screen from "./Step3Screen";
 import Step4Screen from "./Step4Screen";
+import ProgressBar from "./Progressbar";
+import logo from "../assets/logo.png";
+import "./styles/Home.css";
 const Home = () => {
   const [page, setPage] = useState(0);
   const [formData, setFormData] = useState({
@@ -13,6 +16,18 @@ const Home = () => {
     teamSize: "",
   });
   const PageTitles = ["Step1", "Step2", "Step3", "Step4"];
+  const stepHandler = (page) => {
+    if (page === 3) {
+      return setPage(4);
+    }
+    if (page <= 3) {
+      setPage(page);
+    } else {
+      setPage(1);
+    }
+  };
+
+  let progressWidth = 0;
   const pageDisplay = () => {
     if (page === 0) {
       return <Step1Screen formData={formData} setFormData={setFormData} />;
@@ -25,15 +40,22 @@ const Home = () => {
     }
   };
   return (
-    <div>
+    <div className="container">
       <header>
-        <p>Icon</p>
-        <h4>Eden</h4>
+        <img src={logo} alt="Logo" className="logo"></img>
       </header>
-      <section>Progress bar</section>
+      <section>
+        <ProgressBar
+          page={page}
+          changePage={stepHandler}
+          stepWidth={progressWidth}
+        />
+      </section>
       <main>{pageDisplay()}</main>
       <footer>
         <button
+          type="submit"
+          className="submitButton"
           onClick={() => {
             if (page === PageTitles.length - 1) {
               setPage(0);
@@ -42,7 +64,7 @@ const Home = () => {
             }
           }}
         >
-          Create Workplace
+          {page === PageTitles.length - 1 ? "Launch Eden" : "Create Workspace"}
         </button>
       </footer>
     </div>
